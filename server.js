453a91d614e675fc70a51979e0cb66ab8183690a
Tom Davidson <tom@davidson.me.uk>
2016-07-06 10:45:17 +0100

    Adding New Relic (reporting to New Relic for now)

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 "use strict";
 
+require('newrelic');
 const Hapi = require('hapi');
+const fs = require('fs');
 require('dotenv').config({silent: true});
 
 const server = new Hapi.Server();
 server.connection({
   host: '0.0.0.0',
-  port: parseInt(process.env.PORT, 10) || 80
+  port: parseInt(process.env.PORT, 10) || 443,
+  // tls: {
+  //   key: fs.readFileSync('/etc/letsencrypt/live/example.com/privkey.pem'),
+  //   cert: fs.readFileSync('/etc/letsencrypt/live/example.com/cert.pem')
+  // }
 });
 server.route({
   method: 'GET',
