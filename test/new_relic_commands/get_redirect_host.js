09cbdd5f420c3c6ca554148482f22e5ed03e573c
Tom Davidson <tom@davidson.me.uk>
2016-07-11 13:20:52 +0100

    Unit testing for redirect command

---
+++ test/new_relic_commands/get_redirect_host.js
@@ -0,0 +1,16 @@
+const expect = require("chai").expect;
+require('dotenv').config({silent: true});
+
+describe('new_relic_commands/get_redirect_host', function() {
+  const command = require('../../src/new_relic_commands/get_redirect_host');
+  it('has an exec function', function() {
+    expect(typeof command).to.equal('object');
+    expect(typeof command.exec).to.equal('function');
+  });
+  it('returns an object with a host', function() {
+    result = command.exec();
+    expect(typeof result).to.equal('object');
+    expect(typeof result.return_value).to.equal('string');
+    expect(result.return_value).to.equal(process.env.HOSTNAME);
+  });
+});
