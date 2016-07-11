d187ec0708827c849f2563014c7d7a1b5bdac3c7
Tom Davidson <tom@davidson.me.uk>
2016-07-11 15:43:36 +0100

    start as https if certs exist, if not fall back to http

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const Hapi = require('hapi'),
 require('dotenv').config({silent: true});
 
 const server = new Hapi.Server();
-server.connection({
-  host: '0.0.0.0',
-  port: parseInt(process.env.PORT, 10) || 443,
-  tls: {
-    key: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
-    cert: fs.readFileSync(__dirname + '/cert.pem', 'utf8')
-  }
-});
+try{
+  server.connection({
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 443,
+    tls: {
+      key: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
+      cert: fs.readFileSync(__dirname + '/cert.pem', 'utf8')
+    }
+  });
+  console.log('Found ssl certificates, starting as https');
+}catch(e){
+  server.connection({
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT, 10) || 80,
+  });
+}
 server.route({
   method: 'GET',
   path: '/ping',
